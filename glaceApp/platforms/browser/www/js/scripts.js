6dfc16cb72f8986a6b9be8d8f4f53eeb064972c8
Guillermo Strada <guillermostrada@gmail.com>
2019-01-10 12:27:30 -0300

    init
    inicio

---
+++ glaceApp/platforms/browser/www/js/scripts.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function fadeImg(){
+	//$('#usr').fadeOut();
+	//$('#logo').fadeOut(3000);
+	//$('#logo').fadeIn(3000);
+}
+function iniciar(){
+	setTimeout("location.href='#page1'", 1000);
+}
+
+
